adcfb1b09be18a78e4afa09400cce7e0eb000d9f
TiagoSeverino <tiago@severino.xyz>
2019-05-30 01:02:34 +0100

    Fix makeBotPlay

--- webapp/model/FishGameEngine.php
+++ webapp/model/FishGameEngine.php
@@ -121,58 +121,6 @@ class FishGameEngine {
     }
 
     public function makeBotPlay() {
-        $arrayConta = [];
-        $numCards = $this->getBotCardCount();
-        $factorAmplificacao  = 5;
-        $peso1 = (100/$numCards);
-
-        foreach ($this->_botHand->getHand() as $cardInHand){
-                if(array_key_exists((string)$cardInHand, $arrayConta)){
-                    $arrayConta[(string)$cardInHand]++;
-                }else {
-                    $arrayConta[(string)$cardInHand] = 1;
-                }
-        }
-        $roleta = [];
-        $i = 0;
-        $contaAnterior = 0;
-        foreach ($arrayConta as $contaKey => $valueConta) {
-
-            if($i == 0) {
-                $roleta[$i] = $valueConta * $peso1 * $factorAmplificacao;
-            } else{
-                $roleta[$i] = ($valueConta * $peso1 * $factorAmplificacao) + $contaAnterior;
-            }
-
-            $contaAnterior += $valueConta * $peso1 * $factorAmplificacao;
-            $i++;
-        }
-        $numRand = rand(0,$roleta[$i-1  ]);
-        $iterator = 0;
-        foreach($roleta as $item) {
-            if($numRand < $item){
-                if($iterator != 0){
-                    if ($roleta[$iterator-1] > $numRand){
-                        break;
-                    }
-                } else {
-                    break;
-                }
-            }
-
-            $iterator++;
-        }
-
-        if($iterator==0){
-            $iterator=1;
-        }
-        $j = 0;
-        foreach ($arrayConta as $key => $value) {
-            if ($j == $iterator-1) {
-                return new Card($key);
-            }
-            $j++;
-        }
-        return new Card(-1);
+        return $this->getBotHand()[array_rand($this->getBotHand())];
     }
 }
